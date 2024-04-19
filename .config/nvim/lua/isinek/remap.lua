eda98c417548f650c8c665492585b270be97e9e0
Ivan Sinek <isinek@gmail.com>
2024-04-19 08:17:45 +0200

    nvim: rename keymaps modified

--- .config/nvim/lua/isinek/remap.lua
+++ .config/nvim/lua/isinek/remap.lua
@@ -12,5 +12,10 @@ keymap.set('n', '<leader>y', '"+y')
 keymap.set('v', '<leader>y', '"+y')
 keymap.set('n', '<leader>Y', '"+Y')
 
-keymap.set('n', '<leader>r', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+-- Rename in Block
+keymap.set('n', '<leader>rb', 'vap:s/\\<<C-r><C-w>\\>/<C-r><C-w>/gIc<Left><Left><Left>')
+-- Rename in File
+keymap.set('n', '<leader>rf', ':vimgrep "\\<<C-r><C-w>\\>" %<CR>:cdo s/\\<<C-r><C-w>\\>/<C-r><C-w>/gIc<Left><Left><Left><Left>')
+-- Rename in Project
+keymap.set('n', '<leader>rp', ':vimgrep "\\<<C-r><C-w>\\>" `find . -type f -regex ".*\\\\.\\\\(c\\\\|h\\\\|lua\\\\)$"`<CR>:cdo s/\\<<C-r><C-w>\\>/<C-r><C-w>/gIc<Left><Left><Left><Left>')
 
